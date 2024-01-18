7061f79d22b66e80ebb107f79092a24b00f031d8
Nicolivain <nicolasolivain@live.com>
2024-01-18 20:45:55 +0000

    [Dev] summary sheet

--- portfolio_utils.py
+++ portfolio_utils.py
@@ -38,8 +38,41 @@ def compute_position_value_and_dividend_over_time(asset_order_df: pd.DataFrame) 
     return position_value, dividends_earned
 
 
-def compute_portfolio_returns_over_time(portfolio_oder_df: pd.DataFrame, book_currency: str):
-    asset_orders = portfolio_oder_df.groupby(OrderColumns.sym.value)
+def compute_asset_returns_over_time(
+    asset_order_df: pd.DataFrame, book_currency: str, start_date: dt.date, end_date: dt.date
+):
+    instrument_name, local_ccy = get_company_info(
+        asset_order_df[OrderColumns.sym.value].iloc[0], asset_order_df[OrderColumns.market.value].iloc[0]
+    )
+
+    fx_rate = 1
+    if local_ccy != book_currency:
+        if local_ccy == "GBp" and book_currency == "GBP":
+            # fx_rate = 1 / 100
+            fx_rate /= 100
+
+        elif local_ccy == "GBp":
+            fx_rate = get_forex_rates_series("GBP", book_currency, start_date, end_date) / 100
+        else:
+            fx_rate = get_forex_rates_series(local_ccy, book_currency, start_date, end_date)
+
+    asset_value = (
+        get_historical_prices_with_dates(
+            asset_order_df[OrderColumns.sym.value].iloc[0],
+            asset_order_df[OrderColumns.market.value].iloc[0],
+            start_date,
+            end_date,
+        )
+    ).reset_index()
+    asset_value[YFinanceColumns.date.value] = asset_value[YFinanceColumns.date.value].dt.date
+    asset_value = asset_value.set_index(YFinanceColumns.date.value)[YFinanceColumns.price.value]
+    asset_value = asset_value.rename(instrument_name)
+    asset_value *= fx_rate
+    return asset_value.ffill().pct_change().shift(-1), fx_rate
+
+
+def compute_portfolio_returns_over_time(portfolio_order_df: pd.DataFrame, book_currency: str):
+    asset_orders = portfolio_order_df.groupby(OrderColumns.sym.value)
     portfolio_positions_over_time = pd.DataFrame()
     asset_return_over_time = pd.DataFrame()
     end_date = dt.date.today()
@@ -48,51 +81,28 @@ def compute_portfolio_returns_over_time(portfolio_oder_df: pd.DataFrame, book_cu
         position_value = position_value.rename(asset_name)
         start_date = position_value.index.min()
 
-        instrument_name, local_ccy = get_company_info(
-            asset_order_df[OrderColumns.sym.value].iloc[0], asset_order_df[OrderColumns.market.value].iloc[0]
-        )
-
-        fx_rate = 1
-        if local_ccy != book_currency:
-            if local_ccy == "GBp" and book_currency == "GBP":
-                # fx_rate = 1 / 100
-                fx_rate /= 100
-
-            elif local_ccy == "GBp":
-                fx_rate = get_forex_rates_series("GBP", book_currency, start_date, end_date) / 100
-            else:
-                fx_rate = get_forex_rates_series(local_ccy, book_currency, start_date, end_date)
+        asset_returns, fx_rate = compute_asset_returns_over_time(asset_order_df, book_currency, start_date, end_date)
 
         position_value *= fx_rate
-
-        asset_value = (
-            get_historical_prices_with_dates(
-                asset_name, asset_order_df[OrderColumns.market.value].iloc[0], start_date, end_date
-            )
-        ).reset_index()
-        asset_value[YFinanceColumns.date.value] = asset_value[YFinanceColumns.date.value].dt.date
-        asset_value = asset_value.set_index(YFinanceColumns.date.value)[YFinanceColumns.price.value]
-        asset_value *= fx_rate
 
         portfolio_positions_over_time = pd.concat([portfolio_positions_over_time, position_value], axis=1)
         asset_return_over_time = pd.concat(
             [
                 asset_return_over_time,
-                asset_value.rename(asset_name),
+                asset_returns.rename(asset_name),
             ],
             axis=1,
         )
+
     portfolio_positions_over_time = portfolio_positions_over_time.sort_index().ffill()
-    asset_return_over_time = asset_return_over_time.sort_index().ffill().diff().shift(-1).fillna(0)
+    asset_return_over_time = asset_return_over_time.sort_index().fillna(0)
     portfolio_positions_over_time = portfolio_positions_over_time.fillna(0)
 
     # Normalize by total notional
     z = portfolio_positions_over_time.sum(axis=1)
     portfolio_weights_over_time = portfolio_positions_over_time.div(z.values, axis=0)
 
-    portfolio_return_over_time = (
-        portfolio_weights_over_time * asset_return_over_time.div(portfolio_positions_over_time).fillna(0)
-    ).sum(axis=1)
+    portfolio_return_over_time = (portfolio_weights_over_time * asset_return_over_time.fillna(0)).sum(axis=1)
     return portfolio_return_over_time
 
 
